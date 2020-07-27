0de3f4fe1a3146dedf83b68d3aadbb296ecf3281
ohrim433 <57233121+ohrim433@users.noreply.github.com>
2020-07-27 22:59:55 +0300

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+import { radioPlayerInit } from "./radioPlayer.js";
+import { musicPlayerInit } from "./musicPlayer.js";
+import { videoPlayerInit } from "./videoPlayer.js";
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+const deactivationButton = () => {
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+    temp.style.display = 'none';
+}
+
+playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
+        deactivationButton();
+        btn.classList.add('active');
+        playerBlock[i].classList.add('active');
+    })
+);
